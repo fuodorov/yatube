f6393ba0feec1876a59d35c23e283a099698afe5
fuodorov <fuodorov1998@gmail.com>
2020-12-19 11:22:03 +0700

    Update

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
 from .models import Post, Group
-
+from .forms import PostForm
 
 def index(request):
     latest = Post.objects.all()[:11]
@@ -13,4 +14,15 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()[:12]
     return render(request, "group.html", {"group": group, "posts": posts})
-    
+
+
+@login_required
+def new_post(request):
+    form = PostForm(request.POST or None)
+    if request.method == 'POST':
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect("/")
+    return render(request, 'new_post.html', {'form': form})
