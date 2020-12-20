d6b7aa1b67eadeb65b387d4167c97b1bb59f6a0c
fuodorov <fuodorov1998@gmail.com>
2020-12-20 11:56:32 +0700

    flake8

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 
 from .models import Post, Group
 from .forms import PostForm
+
 
 def index(request):
     latest = Post.objects.all()[:11]
