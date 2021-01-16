db2d18352965f15b334b82a16fd46f5fff8784d3
fuodorov <fuodorov1998@gmail.com>
2021-01-16 11:30:08 +0700

    Update

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -6,4 +6,13 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("group/<slug:slug>/", views.group_posts, name="group"),
     path("new/", views.new_post, name="new_post"),
+    path("<str:username>/", views.profile, name="profile"),
+    path("<str:username>/<int:post_id>/", views.post_view, name="post"),
+    path(
+        "<str:username>/<int:post_id>/edit/",
+        views.post_edit,
+        name="post_edit"
+    ),
+    path("404/", views.page_404, name="404"),
+    path("500/", views.page_500, name="500"),
 ]
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -4,19 +4,19 @@ from django.contrib.auth.decorators import login_required
 
 from posts import constants
 
-from .models import Post, Group
+from .models import Post, Group, User
 from .forms import PostForm
 
 
 def index(request):
-    post_list = Post.objects.select_related('group').order_by('-pub_date')
+    post_list = Post.objects.select_related("group").order_by("-pub_date")
     paginator = Paginator(post_list, constants.posts_per_page)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
     return render(
          request,
-         'index.html',
-         {'page': page, }
+         "index.html",
+         {"page": page, }
      )
 
 
@@ -24,12 +24,12 @@ def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()
     paginator = Paginator(posts, constants.posts_per_page)
-    page_number = request.GET.get('page')
+    page_number = request.GET.get("page")
     page = paginator.get_page(page_number)
     return render(
         request,
         "posts/group.html",
-        {'page': page, 'paginator': paginator, 'group': group}
+        {"page": page, "paginator": paginator, "group": group}
     )
 
 
@@ -43,3 +43,64 @@ def new_post(request):
             post.save()
             return redirect("index")
     return render(request, "posts/new_post.html", {"form": form})
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    posts = author.posts.all()
+    paginator = Paginator(posts, constants.posts_per_page)
+    page_number = request.GET.get("page")
+    page = paginator.get_page(page_number)
+    return render(
+        request,
+        "posts/profile.html",
+        {"page": page, "paginator": paginator, "author": author}
+    )
+
+
+def post_view(request, username, post_id):
+    post = get_object_or_404(Post, author__username=username, pk=post_id)
+    return render(
+        request, "posts/post.html",
+        {"post": post, "author": post.author}
+    )
+
+
+@login_required
+def post_edit(request, username, post_id):
+    post = get_object_or_404(
+        Post,
+        author=request.user,
+        author__username=username,
+        pk=post_id
+    )
+    form = PostForm(
+        request.POST or None,
+        files=request.FILES or None,
+        instance=post
+    )
+    if form.is_valid():
+        form.save()
+        return redirect(
+            "post",
+            username=username,
+            post_id=post_id
+        )
+    return render(
+        request,
+        "posts/new_post.html",
+        {"form": form, "post": post}
+    )
+
+
+def page_404(request, exception):
+    return render(
+        request,
+        "misc/404.html",
+        {"path": request.path},
+        status=404
+    )
+
+
+def page_500(request):
+    return render(request, "misc/500.html", status=500)
