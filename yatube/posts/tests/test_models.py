6a0b53a8c1b3ca252a9259cda8fd2bd2eb1d83ac
fuodorov <fuodorov1998@gmail.com>
2021-01-24 23:30:10 +0700

    Update test_models.py

--- yatube/posts/tests/test_models.py
+++ yatube/posts/tests/test_models.py
@@ -15,10 +15,11 @@ class TestModelGroup(TestCase):
         )
 
     def test_verbose_name(self):
+        get_field = Group._meta.get_field
         field_verbose = {
-            "title": Group._meta.get_field("title").verbose_name,
-            "slug": Group._meta.get_field("slug").verbose_name,
-            "description": Group._meta.get_field("description").verbose_name
+            "title": get_field("title").verbose_name,
+            "slug": get_field("slug").verbose_name,
+            "description": get_field("description").verbose_name
         }
         for value, expect in field_verbose.items():
             with self.subTest(value=value):
@@ -26,10 +27,11 @@ class TestModelGroup(TestCase):
                     self.group._meta.get_field(value).verbose_name, expect)
 
     def test_help_text(self):
+        get_field = Group._meta.get_field
         field_help_text = {
-            "title": Group._meta.get_field("title").help_text,
-            "slug": Group._meta.get_field("slug").help_text,
-            "description": Group._meta.get_field("description").help_text
+            "title": get_field("title").help_text,
+            "slug": get_field("slug").help_text,
+            "description": get_field("description").help_text
         }
         for value, expected in field_help_text.items():
             with self.subTest(value=value):
@@ -59,10 +61,11 @@ class TestModelPost(TestCase):
         )
 
     def test_verbose_name(self):
+        get_field = Post._meta.get_field
         field_verbose = {
-            "text": Post._meta.get_field("text").verbose_name,
-            "author": Post._meta.get_field("author").verbose_name,
-            "group": Post._meta.get_field("group").verbose_name
+            "text": get_field("text").verbose_name,
+            "author": get_field("author").verbose_name,
+            "group": get_field("group").verbose_name
         }
         for value, expected in field_verbose.items():
             with self.subTest(value=value):
@@ -70,10 +73,11 @@ class TestModelPost(TestCase):
                     self.post._meta.get_field(value).verbose_name, expected)
 
     def test_help_text(self):
+        get_field = Post._meta.get_field
         field_verbose = {
-            "text": Post._meta.get_field("text").help_text,
-            "author": Post._meta.get_field("author").help_text,
-            "group": Post._meta.get_field("group").help_text
+            "text": get_field("text").help_text,
+            "author": get_field("author").help_text,
+            "group": get_field("group").help_text
         }
         for value, expected in field_verbose.items():
             with self.subTest(value=value):
